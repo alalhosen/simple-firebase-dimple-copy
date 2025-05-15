8bd0fde54ec72085e8dcf5c82c29a5ddb9791676
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-15 10:19:52 +0600

    googleProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css'
-
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from './firebase/firebase.init';
 function App() {
-  const handleGoogleSignIn = () => {
 
+const googleProvider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+signInWithPopup(auth,googleProvider)
+.then(result =>{
+  console.log(result.user);
+})
+.catch(error => console.log('ERROR', error))
   }
 
   return (
