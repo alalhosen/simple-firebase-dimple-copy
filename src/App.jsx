eefc79f29c28a2a1d154f616e0287eb6932cca02
Alal Hosen <alalhosen1992620120@gmail.com>
2025-05-16 21:16:03 +0600

    btn login with github

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from './firebase/firebase.init';
+import { useState } from 'react';
 function App() {
+  const [user, setUser] = useState(null);
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -9,16 +11,23 @@ const googleProvider = new GoogleAuthProvider();
 signInWithPopup(auth,googleProvider)
 .then(result =>{
   console.log(result.user);
+  setUser(result.user);
 })
 .catch(error => console.log('ERROR', error))
   }
 
   return (
     <>
-     
       <h1>Simple Firebase Dimple copy</h1>
       <button onClick={handleGoogleSignIn}>Login With Google</button>
-     
+      <button>Login With Github</button>
+    {
+      user && <div>
+        <h3>User: {user.displayName}</h3>
+        <p>Email: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>
+    } 
     </>
   )
 }
